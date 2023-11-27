5c339d3c5cea62ff59df0bb872af7174d42c181d
Pascal d'Achard <0309597@student.rocvantwente.nl>
2023-11-27 14:44:39 +0100

    web side

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "../app.css";
+</script>
+
+<div class="bg-gray-700 h-screen fixed w-screen -z-50"></div>
+
+
+<div class="flex flex-col min-h-screen justify-center items-center text-white">
+    <slot/>
+</div>
---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import {onMount} from "svelte";
+
+    let status = "Getting api...";
+    
+    onMount(async () => {
+        const response = await fetch("http://localhost:8000/api/");
+        const data = await response.json();
+        status = data.status;
+    })
+</script>
+
+<h1>Status: {status}</h1>
